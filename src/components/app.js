65ef96486e50319c1633a992690b0a336287b264
maheshneogiPractice <maheshneogi@yahoo.com>
2018-07-24 06:40:32 +0530

    initial commit

---
+++ src/components/app.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import BookList from '../container/book-list';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <BookList />
+      </div>
+    );
+  }
+}
